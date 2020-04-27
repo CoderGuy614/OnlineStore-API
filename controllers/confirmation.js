3dac15c669cc006db33a8bbc73cf0f7d528a25f1
CoderGuy614 <jlutz.110@gmail.com>
2020-04-27 17:31:37 +0800

    continued building email templates

--- controllers/confirmation.js
+++ controllers/confirmation.js
@@ -17,33 +17,43 @@ router.post("/", (req, res) => {
     },
   });
 
+  const items = data.cartProducts.map((obj) => obj.name);
+
   const mailOptionsCustomer = {
     from: "CamboCraftClothing@gmail.com",
     to: data.email,
     subject: "CamboCraft Clothing -- ORDER CONFIRMATION --",
     html: `<h4> Hello ${data.name}!</h4>
-        <p>Thank you for your order!  We have received your information and will contact you to arrange a convenient delivery.</p>`,
-  };
+        <p>Thank you for your order!  We have received your information and will contact you to arrange a convenient delivery.</p>
+        <h4>Your Order Items </h4>
+        <ul> 
+        ${items.map((item, index) => `<li> ${item} </li>`)}
+        </ul>
+        
+            <p>${data.filteredOptions.map(
+              (obj) => obj.name + " " + obj.price
+            )}</p>
 
-  const items = data.cartProducts.map(
-    (obj, index) =>
-      obj.name +
-      " " +
-      data.filteredOptions[index].name +
-      " " +
-      "$ " +
-      data.filteredOptions[index].price
-  );
+        <h4> Your Contact Information </p>
+        <p> Name: ${data.name} </p>
+        <p> Email: ${data.email}</p>
+        <p> Phone: ${data.phone}</p>
+        <p> Location: ${data.location}</p>
+        <p> Message: ${data.message}</p>
+        
+        
+        `,
+  };
 
   const mailOptionsSeller = {
     from: "CamboCraftClothing@gmail.com",
     to: "cambocraftclothing@gmail.com",
     subject: "NEW ORDER RECEIVED",
-    html: `<h4> Customer Name: ${data.name}!</h4>
-    <h4> Customer Email: ${data.email}!</h4>
-    <h4> Customer Phone: ${data.phone}!</h4>
-    <h4> Customer Location: ${data.location}!</h4>
-    <h4> Customer Message: ${data.message}!</h4>
+    html: `<h4> Customer Name: ${data.name}</h4>
+    <h4> Customer Email: ${data.email}</h4>
+    <h4> Customer Phone: ${data.phone}</h4>
+    <h4> Customer Location: ${data.location}</h4>
+    <h4> Customer Message: ${data.message}</h4>
     <h4> Order Items </h4>
     <ul> 
     ${items.map((item) => `<li> ${item} </li>`)}
--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("express").Router();
+const { check, validationResult } = require("express-validator");
 const orders = require("../models/orders");
 
 router.get("/", (req, res) => {
@@ -9,7 +10,56 @@ router.get("/", (req, res) => {
     .then((data) => res.send(data));
 });
 
-router.post("/", (req, res) => {
-  orders.create(req.body).then((data) => res.send(data));
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email address").isEmail(),
+    check(
+      "location",
+      "Please enter a delivery location, i.e. hotel name and room number"
+    )
+      .not()
+      .isEmpty(),
+    check("filteredOptions", "Please choose at lease one item to order")
+      .not()
+      .isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      name,
+      email,
+      phone,
+      location,
+      message,
+      cartProducts,
+      filteredOptions,
+    } = req.body;
+
+    try {
+      const newOrder = await orders.create({
+        name,
+        email,
+        phone,
+        location,
+        message,
+        cartProducts,
+        filteredOptions,
+      });
+      res.json(newOrder);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+
+    // orders.create().then((data) => console.log(data));
+  }
+);
 module.exports = router;
